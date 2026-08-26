f8c2eea3e8d3025bee3a172cde6d97986eae736b
nissumon11 <nissumon13@gmail.com>
2026-08-26 16:55:19 +0600

    Count Number Properties solution

---
+++ src/count-number-problem.ts
@@ -0,0 +1,24 @@
+const countNumberProperties = (numbers: number[]) => {
+  let even = 0;
+  let odd = 0;
+  let positive = 0;
+  let negative = 0;
+  for (const number of numbers) {
+    if (number % 2 === 0) {
+      even++;
+    } else {
+      odd++;
+    }
+
+    if (number > 0) {
+      positive++;
+    } else if (number < 0) {
+      negative++;
+    }
+  }
+  return { even, odd, positive, negative };
+};
+
+console.log(countNumberProperties([-5, 0, 3, -4, 1]));
+
+console.log(countNumberProperties([2, 4, 6, 8, 10]));
