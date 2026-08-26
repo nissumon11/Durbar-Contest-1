8fe3355c2fce71a322f931c4968c1ea4199c5036
nissumon11 <nissumon13@gmail.com>
2026-08-26 17:51:20 +0600

    Extract HTML Body Content problem solution

---
+++ dist/extract-html-body-content.js
@@ -0,0 +1,24 @@
+"use strict";
+function extractBodyContent(htmlString) {
+    const afterBody = htmlString.split("<body>")[1];
+    const bodyContent = afterBody.split("</body>")[0];
+    return bodyContent;
+}
+console.log(extractBodyContent(`<html>
+  <head>
+    <title>My Page</title>
+  </head>
+  <body>
+    <h1>Welcome!</h1>
+    <p>This is my new React app.</p>
+  </body>
+</html>`));
+console.log(extractBodyContent(`<!DOCTYPE html>
+<html>
+  <body>
+    <p>Hello world!</p>
+    <div>
+      <span>Nested content.</span>
+    </div>
+  </body>
+</html>`));
